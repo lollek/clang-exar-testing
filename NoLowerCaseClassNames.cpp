855b87316413fe609ffd31227e589f663de07c75
Olle Kvarnström <olle+git@iix.se>
2016-03-31 12:58:07 +0200

    Begin dNoLowerCastClassNames-check

---
+++ NoLowerCaseClassNames.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+#include "clang/Frontend/FrontendPluginRegistry.h"
+#include "clang/AST/AST.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Sema/Sema.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace std;
+using namespace clang;
+
+namespace {
+
+class LocalConsumer : public ASTConsumer {
+public:
+};
+
+class LocalASTAction : public PluginASTAction {
+public:
+  unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI, llvm::StringRef) override {
+    return llvm::make_unique<LocalConsumer>();
+  }
+
+  bool ParseArgs(CompilerInstance const& CI, vector<string> const& args) {
+    return true;
+  }
+};
+
+class LocalVisitor : public RecursiveASTVisitor<LocalVisitor> {
+public:
+  bool VisitCXXRecordDecl(CXXRecordDecl* decl) {
+    cout << decl->getNameAsString() << endl;
+    return true;
+  }
+};
+
+} // anonymous namespace
+
+static FrontendPluginRegistry::Add<LocalASTAction>
+X("exar-lower", "Checks for class names that start with lowercase");
---
+++ test.cc
@@ -0,0 +1,10 @@
+class badName {
+public:
+  int data;
+};
+
+int main() {
+  badName obj;
+  obj.data = 52;
+  return obj.data;
+}
