9ad373ebabdcc1346219bfa84a1cb4d89fd0b425
Olle Kvarnström <olle+git@iix.se>
2016-04-01 16:04:42 +0200

    Add first test

--- cmake/FindEztest.cmake
+++ cmake/FindEztest.cmake
@@ -4,16 +4,17 @@ find_program(eztest
 )
 
 # TODO: Autofind the /libexar.so
-function(eztest_add test_name test_path)
+function(eztest_add plugin_name test_path)
   file(GLOB eztest_subdirs
     RELATIVE "${test_path}" "${test_path}/*")
 
   foreach(subdir ${eztest_subdirs})
     if(IS_DIRECTORY "${test_path}/${subdir}")
-      add_test("${test_name}/${subdir}"
+      add_test("${plugin_name}/${subdir}"
         ${eztest}
         ${clang}
         "${CMAKE_BINARY_DIR}/libexar.so"
+        ${plugin_name}
         "${test_path}/${subdir}"
       )
     endif()
