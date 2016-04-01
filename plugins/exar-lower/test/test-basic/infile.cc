ff2ef02225664bd73d4d32e7eb0fcd1edd324896
Olle Kvarnström <olle+git@iix.se>
2016-04-01 16:13:19 +0200

    Handle errors in eztest

--- plugins/exar-lower/test/test-basic/infile.cc
+++ plugins/exar-lower/test/test-basic/infile.cc
@@ -1,17 +1,2 @@
-class GadName {
-public:
-  int data;
-};
-
-class badName {
-public:
-  int data;
-};
-
-int main() {
-  badName obj;
-  obj.data = 52;
-  GadName obj2;
-  obj2.data = obj.data;
-  return obj2.data;
-}
+class badName{};
+class GoodName{};
