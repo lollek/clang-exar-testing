c11adf0a62b747b86cadaef0efac518661f235e4
Olle Kvarnström <olle+git@iix.se>
2016-04-01 14:09:27 +0200

    remove references to lit

--- cmake/FindLit.cmake
+++
@@ -1,9 +0,0 @@
-find_program(LIT lit
-  NAMES llvm-lit lit
-)
-
-if(NOT EXISTS "${LIT}")
-  message(FATAL_ERROR "lit: Not installed. Can be installed from "
-                      "https://pypi.python.org/pypi/lit")
-endif()
-
