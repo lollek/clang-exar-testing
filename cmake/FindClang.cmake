c37431b3ef75fc9172602dba5ad3bb6bc6ccad0e
Olle Kvarnström <olle+git@iix.se>
2016-04-04 08:42:14 +0200

    Fix setting clang++ as compiler

--- cmake/FindClang.cmake
+++
@@ -1,7 +0,0 @@
-find_program(clang clang)
-if(NOT EXISTS "${clang}")
-  message(FATAL_ERROR "clang: Not found")
-endif()
-
-message(STATUS "Setting compiler to ${clang}")
-set(CMAKE_CXX_COMPILER ${clang})
